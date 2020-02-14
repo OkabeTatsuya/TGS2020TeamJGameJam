523867f36180c5d58faf4e5120349f4a31c8360e
sa_tatuya <sa_tatuya0101@yahoo.co.jp>
2020-02-14 15:59:00 +0900

    ボムの弾の追加

--- GameJamShooting/Assets/Tatsuya/Scripts/CountDownController.cs
+++ GameJamShooting/Assets/Tatsuya/Scripts/CountDownController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class CountDownController : MonoBehaviour
 {
+    //表示するUIのキャンバス
     public GameObject m_CountDownCanvas;
+    //キャンバスの中のImage
     public GameObject[] m_Images;
+    //現在のカウントダウンの時間
     public float m_CurrentCountDownTime;
+    //最後のスタートを表示する時間
     public float m_startDisplayTime;
+    //現在のスタートの表示した累計時間
     private float m_currentStartDisplayTime;
-    // Start is called before the first frame update
+
     void Start()
     {
+        //キャンバスの中のImageを格納、かつ表示しないもののActiveをfalseに
         m_Images = new GameObject[6];
         for (int i = 0; i < m_CountDownCanvas.transform.childCount; i++)
         {
