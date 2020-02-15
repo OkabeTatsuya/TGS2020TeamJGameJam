ab95c5f0d58f12b6a26174c3554ca1f74e84cbe1
TatsuyaOkabe <okabetatsuyaof@gmail.com>
2020-02-15 15:18:51 +0900

    画像追加　マネージャー変更

--- GameJamShooting/Assets/OkabeTatsuya/Script/GameManager.cs
+++ GameJamShooting/Assets/OkabeTatsuya/Script/GameManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
     public bool m_gameStop = false;
     public bool m_gameEnd = true;
+    public int m_winerPlayerID = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,11 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
             {
                 
             }
+        }
+
+        if (m_gameEnd && Input.GetButtonDown("GamePad" + 1 + "_ButtonView"))
+        {
+            SceneManager.LoadScene("TitleScene");
         }
     }
 }
