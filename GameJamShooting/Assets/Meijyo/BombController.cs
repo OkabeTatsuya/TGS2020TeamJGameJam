0a1c602f4b34eebf9a9f5082b84c61bfbcb7ee58
sa_tatuya <sa_tatuya0101@yahoo.co.jp>
2020-02-15 14:36:29 +0900

    ボムの弾と本体の結合

---
+++ GameJamShooting/Assets/Meijyo/BombController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController: MonoBehaviour
+{
+    public GameObject m_BombBullet;
+    public int m_BulletCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            GameObject.Find("BombGenerator").GetComponent<BombGenerator>().MinusCurrentCreateCount();
+            Destroy(gameObject);
+            for (int i = 0; i < m_BulletCount; i++)
+            {
+                Instantiate(m_BombBullet,transform.position,Quaternion.identity);
+            }
+        }
+    }
+}
