ce96fc968ac384320bc94ebada172fecd743fe7a
Toomiya <zeal19300017@c-league.jp>
2020-02-15 13:17:50 +0900

    [Add]弾の処理

--- GameJamShooting/Assets/Toomiya/PlayerControl.cs
+++ GameJamShooting/Assets/Toomiya/PlayerControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 
-     public float PlayerSpeed = 1.0f; //playerのスピード
+     public float PlayerSpeed = 3.0f; //playerのスピード
 
 
 
@@ -17,29 +17,39 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //右の処理
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            transform.Translate(PlayerSpeed, 0, 0);
-        }
+        //左スッティク 移動処理
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.Translate(PlayerSpeed * x, PlayerSpeed * y, 0);
 
-        //左の処理
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            transform.Translate(-PlayerSpeed, 0, 0);
-        }
 
-        //上の処理
-        if(Input.GetAxis("Vertical") > 0)
-        {
-            transform.Translate(0, PlayerSpeed, 0);
-        }
 
-        //下の処理
-        if(Input.GetAxis("Vertical") < 0)
-        {
-            transform.Translate(0, -PlayerSpeed, 0);
-        }
+
+        ////左スティク 右の処理
+        //if (Input.GetAxis("Horizontal") > 0)
+        //{
+        //    transform.Translate(PlayerSpeed, 0, 0);
+        //}
+
+        ////左スティク 左の処理
+        //if (Input.GetAxis("Horizontal") < 0)
+        //{
+        //    transform.Translate(-PlayerSpeed, 0, 0);
+        //}
+
+        ////左スティク 上の処理
+        //if (Input.GetAxis("Vertical") > 0)
+        //{
+        //    transform.Translate(0, PlayerSpeed, 0);
+        //}
+
+        ////左スティク 下の処理
+        //if (Input.GetAxis("Vertical") < 0)
+        //{
+        //    transform.Translate(0, -PlayerSpeed, 0);
+        //}
+
+
 
 
 
